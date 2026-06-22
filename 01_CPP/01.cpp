015991bed5b1ab36fbfde63fb3f9085b646e1c28
Anuragkumar17 <anuragkumar9624@gmail.com>
2026-06-22 16:44:36 +0530

    Basic cpp

---
+++ 01_CPP/01.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int age=10;
+
+    //int = 4 bytes
+    //longlong = 8 bytes
+    //int float = 4 bytes
+    //int double = 8 bytes
+    //char = 1 byte
+    //bool = 1 byte
+
+    //-------------------
+    // int marks=61;
+    // int attendance=75;
+    // if(marks>60 && attendance>60){
+    //     cout<<"PASS";
+    // }
+    // else{
+    //     cout<<"FAIL";
+    // }
+
+    // //SWITCH CASE
+    // char grade = 'A';
+    // switch(grade){
+    //     case 'A':
+    //         cout<<"Excellent";
+    //         break;
+    //     case 'B':
+    //         cout<<"Good";
+    //         break;
+    //     default:
+    //         cout<<"Invalid grade";
+    // }
+
+    // //Ternary operator
+    // int x=10;
+    // int y=20;
+    // int max = (x>y)?x:y;
+    // cout<<"Max Num. : "<<max<<endl;
+
+    
+}
