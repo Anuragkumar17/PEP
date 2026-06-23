a741da9cc8b9277252a75838b8a190e4ad2baff3
Anuragkumar17 <anuragkumar9624@gmail.com>
2026-06-23 16:18:30 +0530

    commit

---
+++ 01_CPP/02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// void doublethevalue(int num){ //(int &num){  - We use & to pass by reference when we want to make changes to the original variable
+//     num = num*2;
+//     cout<<"Inside : "<<num<<endl;
+// }
+
+int main(){
+    // // Pass by value & Pass by reference
+    // int num =10;
+    // doublethevalue(num);
+    // cout<<"Outside : "<<num<<endl;
+
+    
+}
