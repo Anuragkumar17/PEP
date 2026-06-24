072936f89440d5d31b8acca2bd6e783494618c78
Anuragkumar17 <anuragkumar9624@gmail.com>
2026-06-24 16:50:02 +0530

    commit

--- 02_DSA/01.cpp
+++ 02_DSA/01.cpp
@@ -9,4 +9,43 @@ int main(){
     // v.pop_back();
     // v.push_back(6);
     // v.clear();
+
+    int arr[] = {12,32,21,1,55,26,77};
+    int key = 53;
+    int result = linerSearch(arr, 7, key);
+    cout << result << endl;
+
+    int sarr[]= {1,2,3,4,5,6,7,8,9};
+    int skey = 5;
+    int sresult = binarySearch(sarr, 9, skey);
+    cout << sresult << endl;
+
+}
+int linerSearch(int arr[], int n, int key){
+    for(int i=0; i<n; i++){
+        if(arr[i] == key && key>20){
+            return 2*i;
+        }
+        else if(arr[i] == key && key<=20){
+            return i/2;
+        }
+    }
+    return -1;
+}
+int binarySearch(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    while(start<=end){
+        int mid = (start+end)/2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] < key){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+    return -1;
 }
---
+++ 02_DSA/02.cpp
@@ -0,0 +1,17 @@
+/*  
+Time Complxity - How much time an algo takes as input size increases
+
+Big O - represents the upper bound of running time - worst case time complexity of an algorithm
+1. O(1) - Constant time complexity -> the time taken by the algorithm is constant and does not depend on the input size
+cout<<"Hello World"<<endl; // O(1)
+for(int i=0; i<5; i++){ // O(5) -> O(1)
+    cout<<i<<endl;
+}
+
+2. O(log n) - Logarithmic time complexity -> the time taken by the algorithm increases logarithmically with the input size
+
+3. O(n) - Linear time complexity -> the time taken by the algorithm is directly proportional to the input size
+for(int i=0; i<n; i++){} // O(n)
+
+4. O(n^2) - Quadratic time complexity -> the time taken by the algorithm is proportional to the square of the input size
+*/
