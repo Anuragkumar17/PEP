462e3f5fd205665c9b91a06b4f2f6c66b2d9c10f
Anuragkumar17 <anuragkumar9624@gmail.com>
2026-07-01 16:55:54 +0530

    commit

---
+++ 03_CPP/01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+string reverse(string s){
+    int i=0, j=s.length()-1;
+    while(i<j){
+        swap(s[i++],s[j--]);
+    }
+    return s;
+}
+
+int main(){
+    string str = "Hello";
+    //cout<<reverse(str);
+    str.
+}
