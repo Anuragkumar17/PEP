7189ed88f057678feca9c481949dcad35ad61c07
Anuragkumar17 <anuragkumar9624@gmail.com>
2026-07-06 16:54:09 +0530

    commit

--- 04_CPP/02.cpp
+++ 04_CPP/02.cpp
@@ -27,6 +27,48 @@ void insertAtEnd(Node* &head, int val){
     temp->next=node;
     node->next=head;
 }
+
+void insertAtBegining(Node* &head, int val){
+    Node* node = new Node(val);
+    if(head==NULL){
+        head=node;
+        head->next=head;
+        return;
+    }
+    Node* temp = head;
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+    node->next=head;
+    temp->next=node;
+
+}
+
+void insertAtPosition(Node* &head, int pos){
+
+}
+
+void deleteAtEnd(Node* &head){
+
+}
+
+void deleteAtBegining(Node* head){
+    if(head==NULL) return;
+    if(head->next==NULL){
+        delete head;
+        head=NULL;
+        return;
+    }
+    Node* temp = head;
+    Node* temp1 = head;
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+    head = head->next;
+    temp->next=head;
+    delete temp1;
+}
+
 int main(){
 
 }
