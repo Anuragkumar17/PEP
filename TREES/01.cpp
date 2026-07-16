088444ee28bc9d0cb4ee21e5ca5a51195ff27544
Anuragkumar17 <anuragkumar9624@gmail.com>
2026-07-16 15:35:19 +0530

    commit

---
+++ TREES/01.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+//Counting the number of nodes in a binary tree using RECURSION.
+int countNodes(Node* root){
+    if(root==NULL) return 0;
+
+    int leftCount = countNodes(root->left);
+    int rightCount = countNodes(root->right);
+    return leftCount + rightCount + 1;
+};
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    cout << "Number of nodes: " << countNodes(root) << endl;
+    return 0;
+}
